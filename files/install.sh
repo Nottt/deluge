7feb1ee47670a2844f84a7c19528cd69b0556527
Nottt <Nottt@users.noreply.github.com>
2017-11-01 05:22:20 -0200

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -2,19 +2,15 @@
 # To do list : Make this a docker container
 ## Install latest deluge & libtorrent and make it ready to go
 
-# Install necessary packages 
+# Compile 
 
-apt install build-essential checkinstall libboost-system-dev libboost-python-dev libboost-chrono-dev libboost-random-dev libssl-dev curl wget tar  -y
-
-# Grab latest release of libtorrent and compile it 
-
-curl -s https://api.github.com/repos/arvidn/libtorrent/releases/latest | grep "lib*.*gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-
-tar xf *gz && rm *gz && cd lib* && ./configure --enable-debug=no --enable-python-binding --with-libiconv && make && make -j$(nproc) && checkinstall && ldconfig
+dpkg -i /root/libtorrent.deb && apt-get install -f && ldconfig
 
 # ADD Deluge PPA to keep always updated and install deluge
 
-# add-apt-repository ppa:deluge-team/ppa -y && apt-get update && apt-get install deluged deluge-web deluge-console -y
+add-apt-repository ppa:deluge-team/ppa -y && apt-get update && apt-get install deluged deluge-web deluge-console -y
+
+
 
 
 
