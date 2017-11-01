82165e0c846e4fa09ec93be01c32bb2135bb2399
Nottt <Nottt@users.noreply.github.com>
2017-11-01 05:54:08 -0200

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -10,7 +10,23 @@ dpkg -i /root/libtorrent.deb && apt-get install -f && ldconfig
 
 add-apt-repository ppa:deluge-team/ppa -y && apt-get update && apt-get install deluged deluge-web deluge-console -y
 
+# Create necessary folders 
 
+mkdir -p /config/data /downloads/complete /downloads/add /downloads/incomplete /downloads/seed 
+
+# Move necessary folders to /config
+
+cp /usr/bin/deluge* /config 
+
+# Create user and set permissions
+
+adduser --disabled-login --no-create-home --gecos "" deluge
+
+
+
+
+# install s6-overlay
+    curl -s -o - -L "https://github.com/just-containers/s6-overlay/releases/download/v1.20.0.0/s6-overlay-amd64.tar.gz" | tar xzf - -C / && \
 
 
 
